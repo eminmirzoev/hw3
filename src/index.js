aa7068d833b583aac010ccbccb691efed94ebbed
eminmirzoev <56017134+eminmirzoev@users.noreply.github.com>
2019-10-12 22:54:11 +0400

    Homework 3 start

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const getProductOrSum = (first, second) => first % 2
+      ? first * second
+      : first + second;
+
+const getMaxOfProductSum = (first, second, third) => {
+    const product = first * second * third;
+    const sum = first * second + third;
+    const product: number 
+    return product > sum ? product + 3 : sum + 3;
+}
