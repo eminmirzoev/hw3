5c08cc33658fcd8dd478c312a6bea82fe2f23eda
eminmirzoev <56017134+eminmirzoev@users.noreply.github.com>
2019-10-14 17:07:56 +0400

    done

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,8 @@
 describe('getProductOrSum', function () {
-    it ('should return * of first and second (6, 4)', () => {
-        const first = 6;
+    it ('should return * of first and second (2, 4)', () => {
+        const first = 2;
         const second = 4;
-        const expected = 24;
+        const expected = 6;
 
         //When
         const actual = getProductOrSum(first, second);
@@ -22,5 +22,42 @@ describe('getProductOrSum', function () {
         //Then
         assert.deepEqual(actual, expected);
     });
+    it ('should return NaN if first undefined (undefined, 2)', () => {
+        const first = undefined;
+        const second = 2;
+        const expected = NaN;
+
+        //When
+        const actual = getProductOrSum(first, second);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    })
+    it ('should return 0 if first 0 (0, 2)', () => {
+        const first = 0;
+        const second = 2;
+        const expected = 2;
+
+        //When
+        const actual = getProductOrSum(first, second);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    })
+    it ('should return negative meaning if first negative (-4, 2)', () => {
+        const first = -4;
+        const second = 2;
+        const expected = -2;
+
+        //When
+        const actual = getProductOrSum(first, second);
+
+        //Then
+        assert.deepEqual(actual, expected);
+    })
 
 });
+
+describe('getMaxOfProductSum = (first, second, third)', () => {
+   
+})
