bc8fd6000519c822d5a0b74585f0833edd0ff6c5
eminmirzoev <56017134+eminmirzoev@users.noreply.github.com>
2019-10-14 16:02:17 +0400

    changes

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,19 @@
-discribe('getProductSum', () => {
-    it ('should return * of first and second (6, 4) ', () => {
+describe('getProductOrSum', function () {
+    it ('should return * of first and second (6, 4)', () => {
         const first = 6;
         const second = 4;
         const expected = 24;
 
         //When
-        const actual = getProductSum(first, second);
+        const actual = getProductOrSum(first, second);
 
         //Then
         assert.deepEqual(actual, expected);
-        });
-
-    it (' should return + of first and second (8, 2)', function () {
+     });
+     
+     it (' should return + of first and second (8, 2)', () => {
         const first = 8;
-        const second = 3;
+        const second = 2;
         const expected = 10;
 
         //When
